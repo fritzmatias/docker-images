cfb283fcd82256e0c8f89cbd36570e20358e8437
Matias D. Frtiz <fritzmatias@gmail.com>
2024-04-19 05:17:15 -0300

    add docker-compose v2 runner

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 LOGDIR=./logs
+# sets docker compose v2
+[ "$RUN_DOCKER_COMPOSE"x = x ] \
+    && docker compose version >/dev/null 2>&1 \
+    && RUN_DOCKER_COMPOSE="docker compose" \
+    && DEFAULT_DOCKER_OPTIONS="--pull --with-dependencies" \
+    && echo using: ${RUN_DOCKER_COMPOSE} 
+
+# sets docker compose v1
+[ "$RUN_DOCKER_COMPOSE"x = x ] \
+    && docker-compose --version >/dev/null 2>&1 \
+    && RUN_DOCKER_COMPOSE="docker-compose" \
+    && echo runner: ${RUN_DOCKER_COMPOSE} 
+
+[ "$RUN_DOCKER_COMPOSE"x = x ] && echo "Error: docker compose command not found " >&2 && return 1
+
 [ -d $LOGDIR ] || mkdir -p "$LOGDIR"
 
 exp(){
@@ -13,13 +28,14 @@ build(){
 local environment=$1; shift
 local images="$@"
 local opt="$DOCKER_OPTIONS"
+[ "${opt}"x = x ] && opt="$DEFAULT_DOCKER_OPTIONS"
 
     echo "Running build with options: $opt for images: $images. Override options via 'export DOCKER_OPTIONS=opt1 opt2'"
     [ "$environment"x != x ] && [ -f "$environment" ] || (echo "file not found: $environment" && exit 1)
     for image in $images; do
     local log="${LOGDIR}/${image}.log"
         ( exp "$environment" >"${log}" \
-        && docker-compose build  $opt "$image" >>"${log}" \
+        && ${RUN_DOCKER_COMPOSE} build  $opt "$image" >>"${log}" \
         && echo "$image done." ) \
         || (echo "Error on: $image check log: $log" && false) || return 2
     done;
